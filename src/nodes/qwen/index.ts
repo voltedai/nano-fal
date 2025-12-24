1a05d0cf856289c37d9918f4206469819c9303a1
Anthony M <spartanz51@live.fr>
2025-12-24 11:43:25 +0100

    (add) qwen image edit 2511 (#7)

--- src/nodes/qwen/index.ts
+++ src/nodes/qwen/index.ts
@@ -1,2 +1,3 @@
 export { default as QwenImageEditPlusNode } from './QwenImageEditPlusNode.js'
 export { default as QwenImageLayeredNode } from './QwenImageLayeredNode.js'
+export { default as QwenImageEdit2511Node } from './QwenImageEdit2511Node.js'
